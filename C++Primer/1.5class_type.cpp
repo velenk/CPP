34a72933823c53492a9e6be9b3a13be8f8ca0f36
velenk <38948738+velenk@users.noreply.github.com>
2018-07-14 16:48:09 +0800

    .

--- C++Primer/1.5class_type.cpp
+++ C++Primer/1.5class_type.cpp
@@ -2,10 +2,20 @@
 #include "Sales_item.h"
 
 int main(){
-	Sales_item book;
-	std::cin >> book;
-	std::cout << book << std::endl;
+	//Sales_item book;
+	//std::cin >> book;
+	//std::cout << book << std::endl;
 	
-	return 0;
+	Sales_item item1, item2;
+	std::cin >> item1 >> item2;
+	
+	if (item1.isbn() == item2.isbn()){
+		std::cout << item1 +item2 << std::endl;
+		return 0;
+	} else{
+		std::cout << "Data must refer to same ISBN"
+				  << std::endl;
+		return -1;
+	}
 }
 
