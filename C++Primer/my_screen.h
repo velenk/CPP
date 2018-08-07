4cd1a6ed17116855ddf5495b7504aaa54dfd0dc3
velenk <38948738+velenk@users.noreply.github.com>
2018-08-07 21:39:22 +0800

    .

--- C++Primer/my_screen.h
+++ C++Primer/my_screen.h
@@ -8,12 +8,28 @@ public:
   Screen(pos h, pos w, char c) :
          height(h), width(w), contents(h * w, c) { }
   char Get() {return contents[cursor]}
-  inline Get(pos h, pos w) const;
+  inline char Get(pos h, pos w) const;
+  Screen& Move(pos h, pos w);
+  Screen& Display(std::ostream& os) {
+    DoDisplay(os);
+    return *this;
+  }
+  const Screen& Display(std::ostream os) {
+    DoDisplay(os);
+    return *this;
+  }
 private:
   pos cursor = 0;
   pos height = 0, width = 0;
   std::string contents;
+  void DoDisplay(std::ostream os) const {os << contents;}
 };
-
-
+char Screen::Get(pos h, pos w) const {
+  pos row = h * width;
+  return contents[row + w];
+}
+inline Screen& Move(pos h, pos w) {
+  cursor = h * width + w;
+  return *this;
+}
 #endif
