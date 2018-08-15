9529e9b1f9ec7b55d9478286d977a8a6bb0444a8
velenk <38948738+velenk@users.noreply.github.com>
2018-08-15 15:43:58 +0800

    .

---
+++ C++Primer/7.6static_example.cc
@@ -0,0 +1,6 @@
+#include "example.h"
+//double Example::rate;
+std::vector<double> vec;
+int main() {
+  return 0;
+}
---
+++ C++Primer/example.h
@@ -0,0 +1,10 @@
+#ifndef _EXAMPLE_H_
+#define _EXAMPLE_H_
+#include <vector>
+class Example {
+public:
+  static constexpr double rate = 6.5;
+  static const int vec_size = 20;
+  std::vector<double> vec/*(vec_size)*/;
+};
+#endif
